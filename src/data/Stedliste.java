64a09b61abf5d75f3c1aa5ffaed3cf1087a45417
Lars Smeby <s171200@stud.hio.no>
2011-04-12 16:49:41 +0200

    Skrev data og listeklasser

--- src/data/Stedliste.java
+++ src/data/Stedliste.java
@@ -1,12 +1,70 @@
 package data;
 
 import java.io.Serializable;
+import java.util.*;
 
 /**
- * @author Gruppe 3
- *
+ * @author	Gruppe 3
+ * @version	1
+ * @since	1.6
  */
 public class Stedliste implements Serializable
 {
-
+	private static final long serialVersionUID = 4783661286758461364L;
+	private ArrayList<Sted> liste = new ArrayList<Sted>();
+	
+	/**
+	 * Setter inn et sted-objekt bakerst i listen
+	 * @param s	Sted-objekt
+	 */
+	public void settInn(Sted s)
+	{
+		liste.add(s);
+		sorter();		
+	}
+	
+	/**
+	 * Sorterer listen etter stedsnavn
+	 */
+	public void sorter()
+	{
+		Collections.sort(liste, new Stedsammenligner(true));
+	}
+	
+	/**
+	 * Finner et sted-objekt hvis det finnes i listen
+	 * @param navn	Navn på sted
+	 * @return Et sted-objekt hvis det finnes, ellers null
+	 */
+	public Sted finnSted(String navn)
+	{
+		int index = Collections.binarySearch(liste, new Sted(navn,0), new Stedsammenligner(false));
+		if(index<0)
+			return null;
+		return liste.get(index);
+	}
+	
+	/**
+	 * Finner et sted-objekt hvis det finnes i listen
+	 * @param navn	Navn på sted
+	 * @param fylke	Fylket stedet ligger i (SSBs fylkesnummer)
+	 * @return Et sted-objekt hvis det finnes, ellers null
+	 */
+	public Sted finnSted(String navn, int fylke)
+	{
+		int index = Collections.binarySearch(liste, new Sted(navn, fylke), new Stedsammenligner(true));
+		if(index<0)
+			return null;
+		return liste.get(index);
+	}
+	
+	/**
+	 * Sletter et sted og alle dets data fra listen (Alle værdata går også tapt)
+	 * @param s Sted-objekt
+	 */
+	public void slettSted(Sted s)
+	{
+		liste.remove(s);
+		sorter();
+	}
 }
