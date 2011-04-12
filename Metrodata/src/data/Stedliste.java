638e4e2b4d859f020f21872359d62ff9216b7131
Lars Smeby <s171200@stud.hio.no>
2011-04-12 11:13:29 +0200

    La til en del nye klasser

---
+++ Metrodata/src/data/Stedliste.java
@@ -0,0 +1,12 @@
+package data;
+
+import java.io.Serializable;
+
+/**
+ * @author Gruppe 3
+ *
+ */
+public class Stedliste implements Serializable
+{
+
+}
---
+++ Metrodata/src/gui/Metrovindu.java
@@ -0,0 +1,13 @@
+package gui;
+
+import java.io.*;
+import javax.swing.*;
+
+/**
+ * @author Gruppe 3
+ *
+ */
+public class Metrovindu extends JFrame implements Serializable
+{
+
+}
