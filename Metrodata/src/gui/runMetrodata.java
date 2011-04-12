0554b3ac481d43267129f31fb15175a238edc966
Lars Smeby <s171200@stud.hio.no>
2011-04-12 10:53:33 +0200

    Opprettet packager og run-klasse

---
+++ Metrodata/src/gui/runMetrodata.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gui;
+
+/**
+ * @author Eier
+ *
+ */
+public class runMetrodata {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
