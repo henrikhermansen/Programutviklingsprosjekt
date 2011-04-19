25d8483dc61da6c77f287fc9a82b96f1839772f2
Baard <locoboa@gmail.com>
2011-04-19 11:59:59 +0200

    La til vaerRegPanel.java

---
+++ gui/VaerRegPanel.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class VaerRegPanel extends JPanel
+{
+	public static JPanel panel;
+	private JTextField minTemp, maxTemp, nedbør, sted, dato;
+	
+	
+}
