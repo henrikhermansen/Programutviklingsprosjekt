cfbbf96890074899f383d9e38b31d2d00647d2a5
Spearman <spearman_cs@hotmail.com>
2011-04-19 15:04:00 +0200

    Jobbet videre med vaerregpanel

--- gui/VaerRegPanel.java
+++ gui/VaerRegPanel.java
@@ -1,11 +1,14 @@
 package gui;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -18,14 +21,20 @@ public class VaerRegPanel extends JPanel
 	private JComboBox fylke,sted;
 	private JTextField minTemp, maxTemp, nedbør, dato;
 	private JButton registrer;
-	private GridLayout layout;
 	
 	public VaerRegPanel()
 	{
 		panel=new JPanel();
-		panel.setLayout(layout);
+		panel.setLayout(new GridLayout(6,2,3,3));
+		
+		HandlingsLytter handlingslytter=new HandlingsLytter();
+		SkrivefeltLytter skrivefeltlytter=new SkrivefeltLytter();
 		
 		fylke=new JComboBox(Sted.fylkesliste);
+		fylke.addActionListener(handlingslytter);
+		
+		sted=new JComboBox();
+		sted.setEnabled(false);
 		
 		minTemp=new JTextField(10);
 		maxTemp=new JTextField(10);
@@ -35,20 +44,50 @@ public class VaerRegPanel extends JPanel
 		maxTemp.setEditable(false);
 		nedbør.setEditable(false);
 		
-		FeltLytter feltlytter=new FeltLytter();
-		minTemp.addKeyListener(feltlytter);
-		maxTemp.addKeyListener(feltlytter);
-		nedbør.addKeyListener(feltlytter);
+		minTemp.addKeyListener(skrivefeltlytter);
+		maxTemp.addKeyListener(skrivefeltlytter);
+		nedbør.addKeyListener(skrivefeltlytter);
 		
-		registrer
+		registrer=new JButton("Registrer værdata");
+		registrer.setEnabled(false);
+		registrer.addActionListener(handlingslytter);
+		
+		panel.add(new JLabel("Velg fylke"));
+		panel.add(fylke);
+		panel.add(new JLabel("Velg sted"));
+		panel.add(sted);
+		panel.add(new JLabel("Minimumstemperatur"));
+		panel.add(minTemp);
+		panel.add(new JLabel("Maksimumstemperatur"));
+		panel.add(maxTemp);
+		panel.add(new JLabel("Nedbørsmengde i mm"));
+		panel.add(nedbør);
+		panel.add(new JLabel(""));
+		panel.add(registrer);
 	}
 	
-	private class FeltLytter implements KeyListener
+	public JPanel getPanel()
+	{
+		return panel;
+	}
+	
+	private class HandlingsLytter implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource()==fylke)
+				return;
+			if(e.getSource()==registrer)
+				return;
+		}
+	}
+	
+	private class SkrivefeltLytter implements KeyListener
 	{
 		public void keyPressed(KeyEvent arg0)
 		{
 			// TODO Auto-generated method stub
-		}public void keyReleased(KeyEvent arg0)
+		}
+		public void keyReleased(KeyEvent arg0)
 		{
 			// TODO Auto-generated method stub
 		}
@@ -56,6 +95,5 @@ public class VaerRegPanel extends JPanel
 		{
 			// TODO Auto-generated method stub
 		}
-		
 	}
 }
