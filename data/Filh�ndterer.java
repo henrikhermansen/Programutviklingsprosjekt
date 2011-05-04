0f545606abfb9d471f722300ae069678b011adc3
Lars Smeby <s171200@stud.hio.no>
2011-05-04 23:09:21 +0200

    Noe smaatteri i filhaandterer og statistikk-klassene

--- data/Filh�ndterer.java
+++ data/Filh�ndterer.java
@@ -1,15 +1,11 @@
 package data;
 
-import java.io.Serializable;
-
 /**
  *	@author		Gruppe 3
  *	@version	1
  *	@since		1.6
  */
-public class Filhåndterer implements Serializable
+public class Filhåndterer
 {
-	private static final long serialVersionUID = 8810524507410463196L;
-	
 	//Denne klassen laster og lagrer data fra/til fil. Static-metoder.
 }
--- logic/Statistikk.java
+++ logic/Statistikk.java
@@ -1,12 +1,10 @@
 package logic;
 
-import java.io.Serializable;
-
 /**
  * @author Gruppe 3
  *
  */
-public class Statistikk implements Serializable
+public class Statistikk
 {
 
 }
