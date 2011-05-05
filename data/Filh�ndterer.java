d92ae83d192f3a54633d15c496d21937e5a02de7
Baard <locoboa@gmail.com>
2011-05-05 12:11:53 +0200

    Laga funskjoner for  lese og lagre til fil i Filhaandterer.java

--- data/Filh�ndterer.java
+++ data/Filh�ndterer.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.io.*;
+
 /**
  *	@author		Gruppe 3
  *	@version	1
@@ -8,4 +10,60 @@ package data;
 public class Filhåndterer
 {
 	//Denne klassen laster og lagrer data fra/til fil. Static-metoder.
+	
+	final String FILNAVN = "Metrodata.dta";
+	
+	public Stedliste lastInnFil()
+	{
+		Stedliste sl;
+		ObjectInputStream inn;
+		
+		try
+		{
+			inn = new ObjectInputStream(new FileInputStream(FILNAVN));
+			sl = (Stedliste)inn.readObject();
+			inn.close();
+		}
+		catch(ClassNotFoundException cnfe)
+		{
+			System.out.println("ClassNotFoundException, oppretter ny fil");
+			sl = new Stedliste();
+		}
+		catch(FileNotFoundException fnfe)
+		{
+			System.out.println("FileNotFoundException, oppretter ny fil");
+			sl = new Stedliste();
+		}
+		catch(IOException ioe)
+		{
+			System.out.println("IOException, oppretter ny fil");
+			sl = new Stedliste();
+		}
+		
+		return sl;
+	}
+	
+	public void lagreFil(Stedliste sl)
+	{
+		ObjectOutputStream ut;
+		
+		try
+		{
+			ut = new ObjectOutputStream(new FileOutputStream(FILNAVN));
+			ut.writeObject(sl);
+			ut.flush();
+			ut.close();
+		}
+		catch(NotSerializableException nse)
+		{
+			System.out.println("NotSerializableException, fikk ikke lagret");
+			System.exit(1);
+		}
+		catch(IOException ioe)
+		{
+			System.out.println("IOException, fikk ikke lagret");
+			System.exit(1);
+		}
+	}
+	
 }
