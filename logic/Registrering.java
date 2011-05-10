490429dd1300fe81ab548c6431aa2b718f2b4624
Baard <locoboa@gmail.com>
2011-05-10 19:00:25 +0200

    Jobbet mer med reigstreringer.java

--- logic/Registrering.java
+++ logic/Registrering.java
@@ -6,6 +6,8 @@ package logic;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 
+import data.Dato;
+import data.Datoliste;
 import data.Sted;
 import data.Stedliste;
 
@@ -14,6 +16,8 @@ import data.Stedliste;
  */
 public class Registrering
 {
+	
+	private static final double MAXMINTEMP = -60, MAXMAXTEMP =  40, MAXNEDBØR = 300;
 	/**
 	 * @author Lars Smeby
 	 * Metode for å registrere sted
@@ -39,33 +43,91 @@ public class Registrering
 	}
 	
 	/**
-	 * Metode som registrerer værdata for et sted
 	 * @author Bård Skeie
-	 * @return String med tilbakemelding på resultat
+	 * Metode som registrerer værdata for et sted.
+	 * @return String med tilbakemelding på resultat.
 	 */
-	public static String registrerData(JTextField min, JTextField max, JTextField ned)
+	public static String registrerData(JTextField min, JTextField max, JTextField ned, Stedliste stedliste, JComboBox navn, JComboBox fylke, JComboBox lår, JComboBox lmåned, JComboBox ldag )
 	{
 		String minTempString = min.getText();
 		String maxTempString = max.getText();
 		String nedbørString = ned.getText();
-		
+				
 		if(minTempString == "" && maxTempString == "" && nedbørString == "")
 			return "Ingen data er skrevet inn";
 		
-		int minTemp;
-		int maxTemp;
-		int nedbør;
+		double minTemp = -100;
+		double maxTemp = -100;
+		double nedbør = -100;
 		
-		if(minTempString != "")
-			minTemp = Integer.parseInt(minTempString);
-		if(maxTempString != "")
-			maxTemp = Integer.parseInt(maxTempString);
-		if(nedbørString != "")
-			nedbør = Integer.parseInt(nedbørString);
+		boolean minT = false;
+		boolean maxT = false;
+		boolean nedB = false;
 		
-		if(maxTemp < minTemp)
+		int år = -100;
+		int måned = lmåned.getSelectedIndex() + 1;
+		int dag = -100;
+		
+		try
+		{
+			år = Integer.parseInt((String)lår.getSelectedItem());
+			dag = Integer.parseInt((String)ldag.getSelectedItem());
+		}
+		catch(NumberFormatException nfe)
+		{
+			return "Ukjent programfeil (B002)";
+		}
+		
+		try
+		{
+			if(minTempString != "")
+			{
+				minTemp = Double.parseDouble(minTempString);
+				minT = true;
+			}
+			if(maxTempString != "")
+			{
+				maxTemp = Double.parseDouble(maxTempString);
+				maxT = true;
+			}
+			if(nedbørString != "")
+			{
+				nedbør = Double.parseDouble(nedbørString);
+				nedB = true;
+			}
+		}
+		
+		catch(NumberFormatException nfe)
+		{
+			return "Feil i tallformatet";
+		}
+		
+		/**
+		 * Sjekker at innskrevne verdier er innenfor lovlige grenser 
+		 * før registrering kan utføres.
+		 */
+		if(nedB && ((nedbør < 0) || (nedbør > MAXNEDBØR)))
+			return "Nedbør kan ikke være negativ eller over " + MAXNEDBØR + ".";
+		if(minT && maxT && minTemp > maxTemp)
 			return "Makstemperatur kan ikke være større enn minimumstemperatur";
+		if(minT && maxT && ((maxTemp < MAXMINTEMP || maxTemp > MAXMAXTEMP) || (minTemp < MAXMINTEMP || minTemp > MAXMAXTEMP)))
+			return "Temperaturer må være mellom " + MAXMINTEMP + " og " + MAXMAXTEMP + ".";
 		
-		return "HEi";
+		String n = (String) navn.getSelectedItem();
+		Sted sted = stedliste.finnSted(n, fylke.getSelectedIndex());
+		if(sted == null)
+			return "Ukjent programfeil! (B001)";
+		
+		Datoliste dl = sted.getDatoliste();
+		Dato d = dl.finnDato(år, måned, dag);
+		if(d == null)
+		{
+			d = new Dato(dag, måned, år);
+			dl.settInn(d);
+		}
+		
+		
+		
+		return "Hei";
 	}
 }
